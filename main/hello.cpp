ec7216535ce521898c218e8744aedda473076d92
Meeraja <147638760+Meeraja-K@users.noreply.github.com>
2025-03-12 22:29:12 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, World!" << endl;
+  cout << "Hello, Jenkins!" << endl;
+  cout << "Continuous Integration is working perfectly! 🚀" << endl;
+  cout << "This is a new change to test the webhook trigger!" << endl;
   return 0;
 }
